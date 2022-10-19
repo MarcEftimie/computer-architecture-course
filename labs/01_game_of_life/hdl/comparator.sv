fbd91a58bb41932544cc8633c56424650442be8a
Raiyan <verdant.rs@gmail.com>
2022-10-19 16:30:04 -0400

    added comments

--- labs/01_game_of_life/hdl/comparator.sv
+++ labs/01_game_of_life/hdl/comparator.sv
@@ -8,10 +8,13 @@
 module comparator(live, neighbors, out);
 
 input wire live;
-input wire [2:0] neighbors;
-output logic out;
+input wire [2:0] neighbors; //3 bit number that represents total number of cells alive next to the current cell
+output logic out;           //New state of the cell
 
 always_comb begin
+  //Conditions of Conway's game of life
+  //First, if the cell is alive and has exactly 2 alive neighbors it stays alive
+  //Second if 3 alive cells neighbor the cell the cell is alive
   out = (live & ~neighbors[2] & neighbors[1] & ~neighbors[0]) | (~neighbors[2] & neighbors[1] & neighbors[0]);
 end
 
--- labs/01_game_of_life/hdl/conway_cell.sv
+++ labs/01_game_of_life/hdl/conway_cell.sv
@@ -10,25 +10,29 @@ input wire state_0;
 output logic state_d; // NOTE - this is only an output of the module for debugging purposes. 
 output logic state_q;
 
-input wire [7:0] neighbors;
+input wire [7:0] neighbors; //states of each neighboring cell
 
 logic [2:0] sum;
 logic c_out;
+//Takes in 8 1 bit numbers that each represent the state of neighboring cell
+//and outputs a 3 bit sum and carry
 adder_8_1_bits ADDER_8_1(
     .a(neighbors),
     .sum(sum),
     .c_out(c_out)
     );
+//Takes the current state of the cell and the sum of neigboring cells
+//To find the new state of the current cell.
 comparator COMPARATOR(
     .live(state_q),
     .neighbors(sum),
     .out(state_d)
     );
-always_ff @(posedge clk ) begin
-    if (rst == 1) begin
+always_ff @(posedge clk ) begin //on the rising edge of the clock
+    if (rst == 1) begin //if reset on set state of the cell back to default state
         state_q <= state_0;
     end
-    else if (ena == 1) begin
+    else if (ena == 1) begin //if enable is on and rst is off state of the cell is updated
         state_q <= state_d;
     end
 end
